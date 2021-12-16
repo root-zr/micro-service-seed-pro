594237bc9aa59a0162ed000d1c030921036a88f1
zhirong <2779419602@qq.com>
2021-12-16 20:20:07 +0800

    初始化父子工程的项目

---
+++ seed-pro-common/src/main/java/com/common/dto/SendToChain.java
@@ -0,0 +1,15 @@
+package com.common.dto;
+
+import lombok.Data;
+
+@Data
+public class SendToChain {
+
+    private String nameFrom;
+
+    private String nameTo;
+
+    private String dateToString;
+
+    private String goodsId;
+}
